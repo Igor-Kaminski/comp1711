96af2f16d02145d6b32aab0f5f58b58d5a8aff72
Igor-Kaminski <91342396+Igor-Kaminski@users.noreply.github.com>
2023-11-02 23:18:42 +0000

    next 2.5 marks secured

--- work.c
+++ work.c
@@ -50,10 +50,15 @@ int main() {
         return 1;
     }
 
+int numRecords=0;
+int buffer_size=100;
+char line_buffer[buffer_size];
 
+while (fgets(line_buffer, buffer_size, file) !=NULL) {
+    numRecords++;
+}
 
-
-
+printf("Number of records in file: %d\n", numRecords);
 
 
 
