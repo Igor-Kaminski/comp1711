a6d05cf56fea621bfc01acea1ba45ab38de8b695
Igor-Kaminski <91342396+Igor-Kaminski@users.noreply.github.com>
2023-11-03 00:57:58 +0000

    final change for grade scope error had to change step to int

--- week1_c_bootcamp1/work.c
+++ week1_c_bootcamp1/work.c
@@ -69,12 +69,14 @@ int main() {
     char date[11];
     char time[6];
     char steps_str[10];
+    int steps_int;
     int line_counter = 0;
 
     // First 3 lines in correct format printed
     while (fgets(line_buffer, buffer_size, file) != NULL && line_counter < 3) {
         tokeniseRecord(line_buffer, ",", date, time, steps_str);
-        printf("%s/%s/%s", date, time, steps_str);
+        steps_int = atoi(steps_str); // Convert to int as had problem with grade scope told me to change it
+        printf("%s/%s/%d\n", date, time, steps_int);
         line_counter++;
     }
 
